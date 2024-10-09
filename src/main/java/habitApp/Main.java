34f491689e7b2d3a176b83547d0210f36b9e5197
levchig737 <levchekarev3@gmail.com>
2024-10-09 15:01:08 +0300

    add User model, UserService, ConsoleApp. User CRUD, admins access

---
+++ src/main/java/habitApp/Main.java
@@ -0,0 +1,12 @@
+package habitApp;
+
+/**
+ * Main класс приложения
+ * Запускает конслольное приложение
+ */
+public class Main {
+    public static void main(String[] args) {
+        ConsoleApp consoleApp = new ConsoleApp();
+        consoleApp.run();
+    }
+}
