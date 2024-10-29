6f129c76142f394656433f58cfbfdbbe2e577014
levchig737 <levchekarev3@gmail.com>
2024-10-29 15:08:59 +0300

    Добавлены зависимости Spring и lombock,удалены aspectJ.

--- src/main/java/org/habitApp/aspects/LoggableAspect.java
+++ src/main/java/org/habitApp/aspects/LoggableAspect.java
@@ -1,24 +1,24 @@
-package org.habitApp.aspects;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-
-@Aspect
-public class LoggableAspect {
-    @Pointcut("within(@org.habitApp.annotations.Loggable *) && execution(* * (..))")
-    public void annotatedByLoggable() {}
-
-    @Around("annotatedByLoggable()")
-    public Object logging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        System.out.println("Calling method " + proceedingJoinPoint.getSignature());
-        long start = System.currentTimeMillis();
-        Object result = proceedingJoinPoint.proceed();
-        long end = System.currentTimeMillis() - start;
-        System.out.println("Execution of method:" + proceedingJoinPoint.getSignature() +
-                " finished. Execution time is " + end + " ms.");
-
-        return result;
-    }
-}
+//package org.habitApp.aspects;
+//
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Pointcut;
+//
+//@Aspect
+//public class LoggableAspect {
+//    @Pointcut("within(@org.habitApp.annotations.Loggable *) && execution(* * (..))")
+//    public void annotatedByLoggable() {}
+//
+//    @Around("annotatedByLoggable()")
+//    public Object logging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+//        System.out.println("Calling method " + proceedingJoinPoint.getSignature());
+//        long start = System.currentTimeMillis();
+//        Object result = proceedingJoinPoint.proceed();
+//        long end = System.currentTimeMillis() - start;
+//        System.out.println("Execution of method:" + proceedingJoinPoint.getSignature() +
+//                " finished. Execution time is " + end + " ms.");
+//
+//        return result;
+//    }
+//}
