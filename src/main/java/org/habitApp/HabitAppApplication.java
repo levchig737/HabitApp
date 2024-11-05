d041f0552c02d1ba2f773692accbcaa77335f8a0
levchig737 <levchekarev3@gmail.com>
2024-11-05 20:15:54 +0300

    Внедрен Spring boot, swagger

---
+++ src/main/java/org/habitApp/HabitAppApplication.java
@@ -0,0 +1,11 @@
+package org.habitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HabitAppApplication.class, args);
+    }
+}
