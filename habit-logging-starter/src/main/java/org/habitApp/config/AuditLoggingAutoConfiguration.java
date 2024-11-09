98b9fd100a02f69c46b34840a35537c234996cda
levchig737 <levchekarev3@gmail.com>
2024-11-10 00:03:56 +0300

    Исправлена ошибка с импортом

--- habit-logging-starter/src/main/java/org/habitApp/config/AuditLoggingAutoConfiguration.java
+++ habit-logging-starter/src/main/java/org/habitApp/config/AuditLoggingAutoConfiguration.java
@@ -1,5 +1,6 @@
 package org.habitApp.config;
 
+import org.habitApp.aspects.AuditLoggingAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
